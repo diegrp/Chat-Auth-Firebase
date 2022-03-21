2086a33fb76291fb8df5881adb6617c081e6e810
Diego Rocha <dkxever@gmail.com>
2022-03-21 20:58:27 -0300

    Create initial structures of components required for the chat auth

---
+++ src/components/SignIn/index.tsx
@@ -0,0 +1,9 @@
+import * as C from "./styles";
+
+export const SignIn = () => {
+  return(
+    <div>
+      ...
+    </div>
+  )
+}
---
+++ src/components/SignOut/index.tsx
@@ -0,0 +1,7 @@
+export const SignOut = () => {
+  return(
+    <div>
+      ...
+    </div>
+  )
+}
