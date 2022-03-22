963107d5bb1ac5ac36bc7177abbd0a021922c517
Diego Rocha <dkxever@gmail.com>
2022-03-21 21:08:13 -0300

    Structuring and organizing our MsgItem

--- src/components/MsgItem/index.tsx
+++ src/components/MsgItem/index.tsx
@@ -40,9 +40,72 @@ export const MsgItem = ( { id, text, photoURL, uid, onDelete }: Props ) => {
     updateMsg(id, updateText, setActiveUpdate);
   }
 
-  return(
-    <div>
-      ...
-    </div>
+return(
+    <C.MsgWrapper 
+      user={uid} 
+      options={options} 
+      activeUpdate={activeUpdate} 
+      text={text}
+    >
+      <img 
+        src={photoURL} 
+        title="Imagem do usuário" 
+        alt="Imagem de perfil do usuário" 
+      />
+
+      {uid === 'sent' && options &&
+        <C.UserOptions options={options}>
+        {!activeUpdate ? (
+          <>
+            <C.IconDelete onClick={() => onDelete(id)}>
+              <span>
+                <IconClose size={15} />
+              </span>
+            </C.IconDelete>
+            <C.IconUpdate onClick={() => setActiveUpdate(true)}>
+              <span>
+                <IconUpdate size={15} />
+              </span>
+            </C.IconUpdate>
+          </>
+        ):(
+          <>
+            <C.IconBack onClick={() => setActiveUpdate(false)}>
+              <span>
+                <IconBack size={15} />
+              </span>
+            </C.IconBack>
+            <C.IconUpdate onClick={updateMsgText}>
+              <span>
+                <IconInsert size={15} />
+              </span>
+            </C.IconUpdate>
+          </>
+        )}
+        </C.UserOptions>
+      }
+      
+      {activeUpdate ? (
+        <C.InputUpdate      
+          type="text"
+          value={updateText}
+          error={updateText !== '' ? false:true}
+          placeholder={`${updateText !== '' ? 'Digite a nova mensagem':'Preencha o campo vazio'}`}
+          onChange={({target}) => setUpdateText(target.value)}
+          onKeyPress={(e) => {
+
+            /* Atualiza a mensagem ao pressionar 'Enter' */
+
+            if(e.key === 'Enter' && updateText !== '' ){
+              updateMsgText();
+            }else if(e.key === 'Enter' && updateText === ''){
+              setActiveUpdate(false);
+            }
+          }}
+        />
+      ):(
+        <p>{text}</p>
+      )}
+    </C.MsgWrapper>
   )
 }
