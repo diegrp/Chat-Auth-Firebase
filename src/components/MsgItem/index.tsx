62f8ec614b82ce9aea586e96ee692e03bda526a0
Diego Rocha <dkxever@gmail.com>
2022-03-21 21:05:09 -0300

    Passing and arranging the props for our MsgItem

--- src/components/MsgItem/index.tsx
+++ src/components/MsgItem/index.tsx
@@ -1,6 +1,16 @@
 import * as C from "./styles";
 
-export const MsgItem = () => {
+type Props = {
+  id: string
+  text?: string
+  photoURL?: string
+  uid: string
+  onDelete: (id: string) => void
+}
+
+// Mensagens do chat
+
+export const MsgItem = ( { id, text, photoURL, uid, onDelete }: Props ) => {
   return(
     <div>
       ...
