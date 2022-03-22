59e8669503fe6242053cdce99a29838fe9fbd4f7
Diego Rocha <dkxever@gmail.com>
2022-03-21 21:11:12 -0300

    Creating google auth function with firebase authentication

--- src/components/SendMessage/index.tsx
+++ src/components/SendMessage/index.tsx
@@ -1,6 +1,37 @@
+import { FormEvent, useState } from "react";
+import { addMsg } from "../../services/firebase";
 import * as C from "./styles";
 
-export const SendMessage = () => {
+// Área de envio da mensagem ao chat
+
+export const SendMessage = ( { scroll }: any  ) => {
+
+  const [ msg, setMsg ] = useState<string>('');
+  const [ empty, setEmpty ] = useState<boolean>(false);
+
+  // Formulário para envio da mensagem
+
+  /* Adiciona a mensagem, reseta e verifica se o campo está vazio */
+
+  const sendMsgText = ( e: FormEvent <HTMLFormElement> ) => {
+
+    e.preventDefault();
+
+    if(msg !== ''){
+      /* Adiciona mensagem no chat */
+      addMsg(msg);
+
+      setMsg('');
+      setEmpty(false);
+
+      /* Desce a barra até o fim ao enviar a mensagem */
+      scroll.current.scrollIntoView({ behavior: 'smooth' })
+
+    }else{
+      setEmpty(true);
+    }
+  }
+
   return(
     <div>
       ...
