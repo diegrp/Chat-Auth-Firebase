97db0a41bc2ec76cdd2dfcee0f57c5d91587a95a
Diego Rocha <dkxever@gmail.com>
2022-03-21 21:37:15 -0300

    Importing key components and verifying user authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import { Chat } from "./components/Chat";
+import { SignIn } from "./components/SignIn";
+import { auth } from "./config/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+
+// Chat Google Auth Firebase
+
 const App = () => {
+
+  const [ user ] = useAuthState(auth);
+
   return(
     <main>
-
+      { user ? <Chat/> : <SignIn/> }
     </main>
   )
 }
