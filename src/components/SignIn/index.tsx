5008ce5e1320b202c941d1937899af738942e139
Diego Rocha <dkxever@gmail.com>
2022-03-21 21:31:39 -0300

    Status, effect and ref hooks with functions using firebase - addMsg

--- src/components/SignIn/index.tsx
+++ src/components/SignIn/index.tsx
@@ -1,6 +1,18 @@
+import { auth } from "../../config/firebase";
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import * as C from "./styles";
 
+// Logar no chat com google authentication
+
 export const SignIn = () => {
+
+  /* Cria autenticação entre o usuário e a google através do firebase */
+
+  const signInWithGoogle = async () => {
+    const provider = new GoogleAuthProvider();
+    await signInWithPopup(auth, provider);
+  }
+
   return(
     <div>
       ...
