908d177ca36fc4c962a1a4996e35375f2a2d34af
Diego Rocha <dkxever@gmail.com>
2022-03-21 21:03:37 -0300

    Styling our Chat component

---
+++ src/components/Chat/styles.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  min-height: 100vh;
+  background-color: var(--background-color-chat);
+`;
+
+export const MsgArea = styled.div`
+  padding: 80px 0 140px 0;
+  display: flex;
+  flex-direction: column;
+`;
