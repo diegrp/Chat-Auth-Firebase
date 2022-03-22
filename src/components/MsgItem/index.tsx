faf29a8803e3061cfeb16b4ce6eea27ad0f47619
Diego Rocha <dkxever@gmail.com>
2022-03-21 21:06:26 -0300

    Status and effect hooks with functions using firebase - updateMsg

--- src/components/MsgItem/index.tsx
+++ src/components/MsgItem/index.tsx
@@ -1,3 +1,7 @@
+import { useEffect, useState } from "react";
+import { updateMsg } from "../../services/firebase";
+import { CloseOutline as IconClose, ArrowBackOutline as IconBack } from "@styled-icons/evaicons-outline";
+import { ArrowCounterclockwise as IconUpdate, Check as IconInsert } from "@styled-icons/bootstrap";
 import * as C from "./styles";
 
 type Props = {
@@ -11,6 +15,31 @@ type Props = {
 // Mensagens do chat
 
 export const MsgItem = ( { id, text, photoURL, uid, onDelete }: Props ) => {
+
+  const [ updateText, setUpdateText ] = useState<string | undefined>(text);
+  const [ activeUpdate, setActiveUpdate ] = useState<boolean>(false);
+  const [ options, setOptions ] = useState<boolean>(false);
+
+  // Habilita as opções de excluir e editar mensagem
+
+  /* Se o usuário for o mesmo que adicionou a mensagem, habilita as opções de editar e excluir */ 
+
+  useEffect(() => {
+
+    if(uid === 'sent'){
+      setOptions(true);
+    }else{
+      setOptions(false);
+    }
+
+  },[uid]);
+
+  /* Atualiza a mensagem e faz algumas verificações */
+
+  const updateMsgText = () => {
+    updateMsg(id, updateText, setActiveUpdate);
+  }
+
   return(
     <div>
       ...
